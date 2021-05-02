0ec9d74612c891d18c0f601640e9efb3bbace683
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-02 22:16:06 +0300

    first commit

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,50 @@
+print("""**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************""")
+menu = """
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+***********************************
+** What would you like to order? **
+***********************************
+"""
+menu_dic = {
+'Wings' :0 ,
+'Cookies'  :0,
+'Spring Rolls' :0,
+'Entrees' :0,
+'Salmon' :0,
+'Steak':0,
+'Meat Tornado' :0,
+'A Literal Garden' :0,
+'Desserts' :0,
+'Ice Cream' :0,
+'Cake' :0,
+'Pie' :0,
+'Drinks' :0,
+'Coffee' :0,
+'Tea' :0,
+'Unicorn Tears' :0
+}
